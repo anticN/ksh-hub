cf4aceedb787a61c6ca8e398fc1898df33f033b0
anticN <anticn@bzz.ch>
2023-06-07 15:44:41 +0200

    add: passwd validation and check if email exists

--- backend/index.js
+++ backend/index.js
@@ -67,33 +67,36 @@ app.post('/signup', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const credentials = email.split("@");
-
-  if (credentials[1] === "student.ksh.ch") {
-    const names = credentials[0].split(".");
-    const schoolClass = req.body.schoolClass;
-    const goodSubjects = req.body.goodSubjects;
-    const badSubjects = req.body.badSubjects;
-    
-    const user = {
-      vorname: names[0],
-      nachname: names[1],
-      email: email,
-      schoolClass: schoolClass,
-      goodSubjects: goodSubjects,
-      badSubjects: badSubjects,
-      coins: 10,
-      amntHelpTaken: 0,
-      amntHelpGiven: 0,
-      ratings: [],
-      warnings: 0,
-      salt: hashpw().salt,
-      hashed: hashpw().hashed
+  if(db.collection("User").countDocuments({email: email}) === 0) {
+    if (credentials[1] === "student.ksh.ch") {
+      const names = credentials[0].split(".");
+      const schoolClass = req.body.schoolClass;
+      const goodSubjects = req.body.goodSubjects;
+      const badSubjects = req.body.badSubjects;
+      
+      const user = {
+        vorname: names[0],
+        nachname: names[1],
+        email: email,
+        schoolClass: schoolClass,
+        goodSubjects: goodSubjects,
+        badSubjects: badSubjects,
+        coins: 10,
+        amntHelpTaken: 0,
+        amntHelpGiven: 0,
+        ratings: [],
+        warnings: 0,
+        salt: hashpw().salt,
+        hashed: hashpw().hashed
+      }
+      addUserToDB(user);
+      res.send(`Hello ${names[0]} ${names[1]}! your acc has been created with hashed password: ${hashpw()}`);
+    } else {
+      res.status(401).send("Du musst deine KSH-Mail verwenden!");
     }
-    addUserToDB(user);
-    res.send(`Hello ${names[0]} ${names[1]}! your acc has been created with hashed password: ${hashpw()}`);
-  } else {
-    res.status(401).send("Du musst deine KSH-Mail verwenden!");
-  }
+} else {
+  res.status(401).send("Diese E-Mail ist bereits registriert!");
+}
 
   function hashpw() {
     const salt = crypto.randomBytes(16).toString("hex")
@@ -103,6 +106,20 @@ app.post('/signup', (req, res) => {
   }
 });
 
+app.post("/validatepw", (req, res) => {
+  const password = req.body.password
+  const email = req.res.email
+  const userSalt = db.collection("User").findOne({email: email}).salt
+  const userHash = db.collection("User").findOne({email: email}).hashed
+
+  const thing = crypto.pbkdf2Sync(password, userSalt, 1000, 64, "sha512").toString("hex")
+
+  if (thing == userHash) {
+    res.send("Usr valid")
+  } else {
+    res.send("Usr invalid")
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
